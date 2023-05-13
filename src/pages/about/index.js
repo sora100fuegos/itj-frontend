38cdd6cad66ccd0cd6400300021e3fbf315bca01
sora100fuegos <cienfuegossilvacarlos@gmail.com>
2023-05-12 18:42:12 -0600

    modal changes added

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,24 +1,22 @@
 
 import PageDescription from "@/components/PageDescription";
-import { Grid, Button } from "@mui/material";
+import { Grid, Button , Stack , Chip } from "@mui/material";
 import { useRouter } from "next/router";
 
-export default function About() {
+export default function About({ skills }) {
   const router = useRouter();
-
   return (
     <section>
       <PageDescription
-        title="About"
+        title="About me"
         description="Here you will find some of the personal and clients projects that I created with each project containing its own case study"
       />
 
       <Grid container spacing={2}>
         <Grid item md={6}>
           <h2>Get to know me!</h2>
-          <p>
-            I'm a Frontend Web Developer building the Front-end of Websites and
-            Web Applications that leads to the success of the overall product.
+          <p>I am  a backend  Developer specialized in java with some QA experience
+            currently studying javascript to immprove my knowledge as a  the Frontend  developer 
             Check out some of my work in the Projects section.
           </p>
           <Button
@@ -30,16 +28,36 @@ export default function About() {
           </Button>
         </Grid>
 
-        <Grid item md={6}></Grid>
+        <Grid item md={6}>
+        <h2>My Skills</h2>
+          <Stack direction="row" spacing={2} useFlexGap flexWrap="wrap">
+            {skills.map((skill) => (
+              <Chip key={skill} label={skill} />
+            ))}
+          </Stack>
+        </Grid>
       </Grid>
     </section>
   );
 }
 
 export async function getStaticProps() {
+  let skills = [];
+
+  try {
+    const response = await fetch(
+      "https://manifest-actor-350500-default-rtdb.firebaseio.com/Skills.json"
+    );
+    const data = await response.json();
+    skills = data.split(",");
+  } catch (error) {
+    console.error(error);
+  }
+
   return {
     props: {
-      skills: ["C", "Javascript", "C#", "React", "Git", "SQL", "Node"],
+      skills,
     },
+    revalidate: 30,
   };
 }
